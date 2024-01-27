b095630fa313e703c146601ef1753f67965dce0f
mahadi570 <mahadihasan18407@gmail.com>
2024-01-27 11:45:24 +0400

    Installed Basic packages and hosted to firebase

---
+++ src/MainLayout/MainLayout.jsx
@@ -0,0 +1,9 @@
+const MainLayout = () => {
+  return (
+    <div>
+      <h1>Something good is going to be happen.</h1>
+    </div>
+  )
+};
+
+export default MainLayout;
---
+++ src/main.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import MainLayout from "./MainLayout/MainLayout";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router}></RouterProvider>
+  </React.StrictMode>
+);
